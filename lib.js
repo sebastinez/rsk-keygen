3fc58caa51bbef0e45aeb13afb4b1d1c85b3f2b9
Sebastinez <sebastinez@me.com>
2019-09-20 15:12:33 -0300

    Deleted static keys

--- lib.js
+++ lib.js
@@ -4,8 +4,6 @@ const mainnet = networks.bitcoin.wif;
 const Wallet = require("./utils/wallet");
 
 let wallet = new Wallet(mainnet);
-/* wallet.privKey = Buffer.from("0829e8a1e7f48944d79ffc85e9344761629dbd8298508b9c6690775da018e5f3", "hex");
-wallet.wifPrivKey = "KwVaba6XJKTZCmsPAZaaV3dKXGpzdtEE3Neh1ehmLvidKfpyry3n"; */
 console.log(
   wallet
     .generateWifPrivateKey()
